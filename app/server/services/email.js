9187d52cc43ff5e6702785f27c3c68cbb8a573b5
Justin Wei <wei.justin.a@gmail.com>
2017-08-19 16:14:00 -0500

    Customized Email copy

--- app/server/services/email.js
+++ app/server/services/email.js
@@ -79,7 +79,7 @@ controller.sendVerificationEmail = function(email, token, callback) {
 
   var options = {
     to: email,
-    subject: "[HACKMIT] - Verify your email"
+    subject: "[HACKDFW] - Verify your email"
   };
 
   var locals = {
@@ -116,7 +116,7 @@ controller.sendPasswordResetEmail = function(email, token, callback) {
 
   var options = {
     to: email,
-    subject: "[HACKMIT] - Password reset requested!"
+    subject: "[HACKDFW] - Password reset requested!"
   };
 
   var locals = {
@@ -157,7 +157,7 @@ controller.sendPasswordChangedEmail = function(email, callback){
 
   var options = {
     to: email,
-    subject: "[HACKMIT] - Your password has been changed!"
+    subject: "[HACKDFW] - Your password has been changed!"
   };
 
   var locals = {
