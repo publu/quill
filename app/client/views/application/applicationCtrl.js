1903d7203012e106b798995444fd5de683e26845
Thomas Hobohm <superman3275@gmail.com>
2017-08-28 01:29:36 -0500

    Migrated over the rest.

--- app/client/views/application/applicationCtrl.js
+++ app/client/views/application/applicationCtrl.js
@@ -13,14 +13,6 @@ angular.module('reg')
       // Set up the user
       $scope.user = currentUser.data;
 
-      // Is the student from MIT?
-      $scope.isMitStudent = $scope.user.email.split('@')[1] == 'mit.edu';
-
-      // If so, default them to adult: true
-      if ($scope.isMitStudent){
-        $scope.user.profile.adult = true;
-      }
-
       // Populate the school dropdown
       populateSchools();
       _setupForm();
